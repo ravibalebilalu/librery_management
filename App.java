4089ff6fce9e5ae2015b0a5052abbd92ba57d03e
ravibalebilalu <81ravikiran@gmail.com>
2023-08-19 13:05:36 +0530

    book

---
+++ App.java
@@ -0,0 +1,10 @@
+ 
+import javax.sound.sampled.SourceDataLine;
+
+import Utility.Book;
+
+public class App {
+ public static void main(String[] args) {
+       
+ }   
+}
---
+++ Utility/Book.java
@@ -0,0 +1,46 @@
+/*Book: A class representing a book with attributes 
+like title, author, ISBN, and availability status.
+ */
+package Utility;
+ public class Book{
+    String title;
+    String author;
+    String isbn;
+    boolean availability;
+
+    //title
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    public String getTitle(){
+         return title;
+    }
+    //author
+    public void setAuthor(String author){
+        this.author = author;
+    }
+
+    public String getAuthor(){
+        return author;
+    }
+
+    //isbn
+    public void setIsbn(String isbn){
+        this.isbn = isbn;
+    }
+
+    public String getIsbn(){
+        return isbn;
+    }
+    //availability
+    public void  setAvailability(boolean availability){
+        this.availability = availability;
+    }
+
+    public boolean getAvailability(){
+        return availability;
+    }
+ 
+
+}
